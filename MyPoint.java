6170c5d5a44fe7b6bb673e6e054495fc2c894050
sara woodford <sarawoodford6@gmail.com>
2024-10-02 10:52:17 -0230

    created files and copied previously done MyPoint
    MyPoint was created in a previous weekly exercise

---
+++ MyPoint.java
@@ -0,0 +1,56 @@
+public class MyPoint {
+    
+        private int x = 0;
+        private int y = 0;
+        
+        public MyPoint(){
+    
+        }
+    
+        public MyPoint(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    
+        public int getX(){
+            return x;
+        }
+    
+        public int getY(){
+            return y;
+        }
+    
+        public void setX(int x){
+            this.x = x;
+        }
+    
+        public void setY(int y){
+            this.x = y;
+        }
+    
+        public void setXY(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    
+        public String toString(){
+            return "(" + x + "," + y + ")";
+        }
+    
+        public double distance(int x, int y){
+            int xDiff = this.x - x;
+            int yDiff = this.y - y;
+            return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+        }
+    
+        public double distance(MyPoint another){
+            int xDiff = this.x - another.x;
+            int yDiff = this.y - another.y;
+            return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+        }
+    
+        public double distance(){
+            return Math.sqrt(this.x * this.x + this.y * this.y);
+        }
+    }
+
