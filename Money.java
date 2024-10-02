49eeb4512a699484500f450cde6f01d0cd67b128
sara woodford <sarawoodford6@gmail.com>
2024-10-02 19:25:03 -0230

    creating money class
    assigned attributes, contructors, methods and displayed
    results
    
    researched examples of new methods long, equals and compareTo

--- Money.java
+++ Money.java
@@ -1,3 +1,47 @@
 public class Money {
-    
+    private long dollars;
+    private long cents;
+
+    public Money(double amount){
+        this.dollars = (long) amount;
+        this.cents = (long) ((amount - this.dollars) * 100 );
+    }
+
+    public Money(Money otherObject){
+        this.dollars = otherObject.dollars;
+        this.cents = otherObject.cents;
+    }
+
+    public Money add(Money otherAmount){
+        long totalCents = this.cents + otherAmount.cents;
+        long extraDollars = totalCents / 100;
+        long newCents = totalCents % 100;
+        long newDollars = this.dollars + otherAmount.dollars + extraDollars;
+        return new Money(newDollars + newCents / 100);
+    }
+
+    public Money subtract(Money otherAmount){
+        long totalCents1 = this.dollars * 100 + this.cents;
+        long totalCents2 = otherAmount.dollars * 100 + otherAmount.cents;
+        long centDiff = totalCents1 - totalCents2;
+        return new Money(centDiff / 100);
+    }
+
+    public int compareTo(Money otherObject){
+        if (this.dollars > otherObject.dollars || this.dollars == otherObject.dollars && this.cents > otherObject.cents){
+            return  1;
+        } else if(this.dollars < otherObject.dollars || this.dollars == otherObject.dollars && this.cents < otherObject.cents){
+            return -1;
+        } else{
+            return 0;
+        }
+    }
+
+    public boolean equals(Money otherObject){
+        return this.dollars == otherObject.dollars && this.cents == otherObject.cents;
+    }
+
+    public String toString(){
+        return ("$" + this.dollars + "." + String.format("%02d",this.cents));
+    }
 }
