e7c8583f809ec9f3ce362fc288f7de528d181051
sara woodford <sarawoodford6@gmail.com>
2024-10-02 11:14:52 -0230

    completing MyLine
    assigned attributes, acccessed and changed data using
    getters and setters, used methods to do required
    calculations and finally displayed results.

--- MyLine.java
+++ MyLine.java
@@ -1,3 +1,50 @@
 public class MyLine {
-    
+    // assign attributes 
+    private MyPoint begin;
+    private MyPoint end;
+
+    // constructors 
+    public MyLine(MyPoint end, MyPoint begin){
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public MyLine(int x1, int y1, int x2, int y2){
+        begin = new MyPoint(x1, y1);
+        end = new MyPoint(x2, y2);
+    }
+
+    // getters and setters 
+    public MyPoint getBegin(){
+        return begin;
+    }
+
+    public MyPoint getEnd(){
+        return end;
+    }
+
+    public void setBegin(MyPoint begin){
+        this.begin = begin;
+    }
+
+    public void setEnd(MyPoint end){
+        this.end = end;
+    }
+
+    // methods 
+
+    public double getLength(){
+        return Math.hypot(end.getX() - begin.getX(), end.getY() - begin.getY());
+    }
+
+    public double getGradient(){
+        double xDiff = end.getX() - begin.getX();
+        double yDiff = end.getY() - begin.getY();
+        return Math.atan2(yDiff, xDiff);
+    }
+
+    public String toString(){
+        return "My Line [begin= " + begin + ", end= " + end + "]";
+    }
+
 }
